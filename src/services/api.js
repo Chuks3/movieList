9b38d435bb5386ed5fd24c4b25c14d5196a30382
Chuks <chuksonyekams@gmail.com>
2025-08-17 15:06:35 +0100

    First Commit

---
+++ src/services/api.js
@@ -0,0 +1,14 @@
+const API_KEY = "14bbe32109e84ccfdd8056ac86eea97a";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+    const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`)
+    const data = await response.json()
+    return data.results
+};
+
+export const searchMovies = async (query) => {
+    const response = await fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`)
+    const data = await response.json()
+    return data.results
+};
